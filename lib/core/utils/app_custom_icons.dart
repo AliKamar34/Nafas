fc2638f43bdfdb5ceb62500353bad96dc8dad80e
Ali kamar <aliekamar123@gmail.com>
2025-02-02 21:32:41 +0200

    add custom icons class

---
+++ lib/core/utils/app_custom_icons.dart
@@ -0,0 +1,5 @@
+abstract class AppCustomIcons {
+  static const mailIon = 'assets/images/icons/mail.svg';
+  static const lockIcon = 'assets/images/icons/lock-on.svg';
+  static const googleIcon = 'assets/images/icons/flat-color-icons_google.svg';
+}
