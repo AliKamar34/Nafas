4e669a1caddbc37f83f0c7a6c1f5723aa11124b3
Ali kamar <aliekamar123@gmail.com>
2025-02-04 23:06:56 +0200

    feat: add sign-up form widget and integrate into sign-up view

---
+++ lib/features/auth/presentation/views/widgets/sign_up_form.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:nafas_app/core/utils/app_custom_icons.dart';
+import 'package:nafas_app/core/widgets/custom_button.dart';
+import 'package:nafas_app/features/auth/presentation/views/widgets/custom_text_field.dart';
+
+class SingUpForm extends StatelessWidget {
+  const SingUpForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          CustomTextField(
+            lable: 'الاسم بالكامل ',
+            hint: 'الاسم بالكامل ',
+            icon: AppCustomIcons.humanIcon,
+            validator: (value) {
+              if (value == null) {
+                return 'الرجاء إدخال الاسم بالكامل';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 12),
+          CustomTextField(
+            lable: 'البريد الإلكتروني',
+            hint: 'mail@example.com',
+            icon: AppCustomIcons.mailIon,
+            validator: (value) {
+              if (value == null) {
+                return 'الرجاء إدخال البريد الإلكتروني';
+              } else if (!value.contains('@') || !value.contains('.')) {
+                return 'الرجاء إدخال بريد إلكتروني صحيح';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 12),
+          CustomTextField(
+            lable: 'كلمة المرور',
+            hint: '********',
+            icon: AppCustomIcons.lockIcon,
+            suffixIcon: AppCustomIcons.hideIcon,
+            validator: (value) {
+              if (value == null) {
+                return 'الرجاء إدخال كلمة المرور';
+              } else if (value.length < 8) {
+                return 'كلمة المرور يجب أن تكون أكثر من 8 أحرف';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 12),
+          CustomTextField(
+            lable: 'تأكيد كلمة المرور',
+            hint: '********',
+            icon: AppCustomIcons.lockIcon,
+            suffixIcon: AppCustomIcons.hideIcon,
+            validator: (value) {
+              if (value == null) {
+                return 'الرجاء إدخال تأكيد كلمة المرور';
+              } else if (value.length < 8) {
+                return 'كلمة المرور يجب أن تكون أكثر من 8 أحرف';
+              }
+              // else if (value != 'كلمة المرور') {
+              //   return 'كلمة المرور غير متطابقة';
+              // }
+              return null;
+            },
+          ),
+          const SizedBox(height: 12),
+          CustomButton(
+            text: 'إنشاء حساب',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
