5a75784787394ce260bcb6c321febf4c787cf236
Ali kamar <aliekamar123@gmail.com>
2025-02-06 21:56:34 +0200

    feat: add new SVG icons and update app_custom_icons

--- lib/core/utils/app_custom_icons.dart
+++ lib/core/utils/app_custom_icons.dart
@@ -4,4 +4,8 @@ abstract class AppCustomIcons {
   static const googleIcon = 'assets/images/icons/flat-color-icons_google.svg';
   static const humanIcon = 'assets/images/icons/shape.svg';
   static const hideIcon = 'assets/images/icons/hide.svg';
+  static const counterIcon = 'assets/images/icons/counter.svg';
+  static const homeIcon = 'assets/images/icons/home.svg';
+  static const newsPaperIcon = 'assets/images/icons/newspaper.svg';
+  static const lasticonInNavBar = 'assets/images/icons/last_icon_in_navbar.svg';
 }
