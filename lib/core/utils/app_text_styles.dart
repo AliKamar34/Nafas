cea58020b1e5221de763b5460772b94d3f7a21e8
Ali kamar <aliekamar123@gmail.com>
2025-02-07 01:55:08 +0200

    add new icons

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -8,6 +8,13 @@ abstract class TextStyles {
     );
   }
 
+  static TextStyle bold16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
   static TextStyle bold18(context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 18),
@@ -75,7 +82,7 @@ abstract class TextStyles {
 double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
   double scaleFactor = getScaleFactor(context);
   double responsiveFontSize = fontSize * scaleFactor;
-  double lowerLimit = fontSize * 0.6;
+  double lowerLimit = fontSize * 0.7;
   double upperLimit = fontSize * 1.2;
   return responsiveFontSize.clamp(lowerLimit, upperLimit);
 }
@@ -83,8 +90,8 @@ double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
 double getScaleFactor(BuildContext context) {
   double width = MediaQuery.sizeOf(context).width;
   if (width < 800) {
-    return width / 600;
+    return width / 550;
   } else {
-    return width / 900;
+    return width / 1000;
   }
 }
