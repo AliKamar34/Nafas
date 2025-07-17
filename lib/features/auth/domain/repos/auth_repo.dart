97d4467fad2d30de9c6ce050d1f53c381619fd53
Ali kamar <aliekamar123@gmail.com>
2025-07-17 22:00:37 +0300

    add UserEntity and AuthRepo classes for user authentication

---
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:nafas_app/core/errors/failure.dart';
+import 'package:nafas_app/features/auth/domain/entities/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String name,
+    String email,
+    String password,
+  );
+
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword(
+    String email,
+    String password,
+  );
+
+  Future<Either<Failure, UserEntity>> signInWithGoogle();
+
+  Future addUserData({required UserEntity user});
+
+  Future saveUserData({required UserEntity user});
+
+  Future<UserEntity> getUserData({required String uId});
+}
