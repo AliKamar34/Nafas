5c0331f608830ac61e3e6105e3e7421d948b4dc9
Ali kamar <aliekamar123@gmail.com>
2025-07-17 22:00:58 +0300

    add UserModel and AuthRepoImpl classes for user authentication and data management

---
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -0,0 +1,174 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:nafas_app/core/errors/exceptions.dart';
+import 'package:nafas_app/core/errors/failure.dart';
+import 'package:nafas_app/core/services/database_service.dart';
+import 'package:nafas_app/core/services/firebase_auth_service.dart';
+import 'package:nafas_app/core/services/shared_preferences_singleton.dart';
+import 'package:nafas_app/core/utils/backend_endpoint.dart';
+import 'package:nafas_app/core/utils/constant.dart';
+import 'package:nafas_app/features/auth/data/models/user_model.dart';
+import 'package:nafas_app/features/auth/domain/entities/user_entity.dart';
+import 'package:nafas_app/features/auth/domain/repos/auth_repo.dart';
+
+class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+  final DatabaseService databaseService;
+
+  AuthRepoImpl({
+    required this.firebaseAuthService,
+    required this.databaseService,
+  });
+
+  @override
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+    String name,
+    String email,
+    String password,
+  ) async {
+    User? user;
+
+    try {
+      user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final userEntity = UserEntity(
+        name: name,
+        email: email,
+        uId: user.uid,
+      );
+
+      final isUserExists = await databaseService.isDataExists(
+        path: BackendEndpoint.isUserExists,
+        documentId: user.uid,
+      );
+
+      if (isUserExists) {
+        await getUserData(uId: user.uid);
+      } else {
+        await addUserData(user: userEntity);
+      }
+
+      await saveUserData(user: userEntity);
+
+      return right(userEntity);
+    } on CustomException catch (e) {
+      await deleteUser(user);
+
+      return left(ServerFailure(e.message));
+    } catch (e) {
+      deleteUser(user);
+
+      log(
+        'Exception in AuthRepoImpl.createUserWithEmailAndPassword: ${e.toString()}',
+      );
+
+      return left(ServerFailure('لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.'));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      var user = await firebaseAuthService.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      var userEntity = await getUserData(uId: user.uid);
+
+      await saveUserData(user: userEntity);
+
+      return right(
+        userEntity,
+      );
+    } on CustomException catch (e) {
+      return left(
+        ServerFailure(e.message),
+      );
+    } catch (e) {
+      log('Exception in AuthRepoImpl.signInWithEmailAndPassword: ${e.toString()}');
+
+      return left(
+        ServerFailure('لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> signInWithGoogle() async {
+    User? user;
+
+    try {
+      user = await firebaseAuthService.signInWithGoogle();
+
+      var userEntity = UserModel.fromFirebaseUser(user);
+
+      var isUserExists = await databaseService.isDataExists(
+        path: BackendEndpoint.isUserExists,
+        documentId: user.uid,
+      );
+
+      if (isUserExists) {
+        await getUserData(uId: user.uid);
+      } else {
+        await addUserData(user: userEntity);
+      }
+
+      await saveUserData(user: userEntity);
+
+      return right(
+        userEntity,
+      );
+    } catch (e) {
+      await deleteUser(user);
+
+      log(
+        'Exception in AuthRepoImpl.signInWithGoogle: ${e.toString()}',
+      );
+
+      return left(
+        ServerFailure('لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.'),
+      );
+    }
+  }
+
+  @override
+  Future addUserData({required UserEntity user}) async {
+    await databaseService.addData(
+      path: BackendEndpoint.addUserData,
+      data: UserModel.fromEntity(user).toMap(),
+      documentId: user.uId,
+    );
+  }
+
+  @override
+  Future saveUserData({required UserEntity user}) async {
+    var jsonData = jsonEncode(UserModel.fromEntity(user).toMap());
+
+    await Prefs.setString(kUserData, jsonData);
+  }
+
+  @override
+  Future<UserEntity> getUserData({required String uId}) async {
+    var userData = await databaseService.getData(
+      path: BackendEndpoint.getUserData,
+      documentId: uId,
+    );
+    return UserModel.fromJson(userData);
+  }
+
+  Future<void> deleteUser(User? user) async {
+    if (user != null) {
+      await firebaseAuthService.deleteUser();
+    }
+  }
+}
