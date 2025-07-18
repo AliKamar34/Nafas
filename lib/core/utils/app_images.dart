78dd363cfa2dd091a991a9e5429b99357995502e
Ali kamar <aliekamar123@gmail.com>
2025-07-18 21:10:50 +0300

    refactor: update icon asset paths and improve text style methods for consistency

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,9 +2,89 @@
 class Assets {
   Assets._();
 
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  static const String imagesArticleTestImage =
+      "assets/images/article_test_image.png";
+
+  static const String imagesCigarette = "assets/images/cigarette.png";
+
+  static const String imagesConnectionError =
+      "assets/images/connection_error.png";
+
+  static const String imagesCup = "assets/images/cup.svg";
+
+  static const String imagesError = "assets/images/error.png";
+
+  static const String imagesGoal = "assets/images/Goal.png";
+
+  static const String imagesIconsAccountDataIcon =
+      "assets/images/icons/account_data_icon.svg";
+
+  static const String imagesIconsBell = "assets/images/icons/bell.svg";
+
+  static const String imagesIconsBlueHuman =
+      "assets/images/icons/blue_human.svg";
+
+  static const String imagesIconsChangePasswordIcon =
+      "assets/images/icons/change_password_icon.svg";
+
+  static const String imagesIconsCounter = "assets/images/icons/counter.svg";
+
+  static const String imagesIconsDeleteAccountIcon =
+      "assets/images/icons/delete_account_icon.svg";
+
+  static const String imagesIconsForgotPassword =
+      "assets/images/icons/forgot_password.svg";
+
+  static const String imagesIconsGoogleIcon =
+      "assets/images/icons/google_icon.svg";
+
+  static const String imagesIconsHide = "assets/images/icons/hide.svg";
+
+  static const String imagesIconsHome = "assets/images/icons/home.svg";
+
+  static const String imagesIconsLastIconInNavbar =
+      "assets/images/icons/last_icon_in_navbar.svg";
+
+  static const String imagesIconsLockOn = "assets/images/icons/lock-on.svg";
+
+  static const String imagesIconsLogOutIcon =
+      "assets/images/icons/log_out_icon.svg";
+
+  static const String imagesIconsMail = "assets/images/icons/mail.svg";
+
+  static const String imagesIconsMyAccountIcon =
+      "assets/images/icons/my_account_icon.svg";
+
+  static const String imagesIconsNewspaper =
+      "assets/images/icons/newspaper.svg";
+
+  static const String imagesIconsNightMoodIcon =
+      "assets/images/icons/night_mood_icon.svg";
+
+  static const String imagesIconsPrivacyIcon =
+      "assets/images/icons/privacy_icon.svg";
+
+  static const String imagesIconsRulesIcon =
+      "assets/images/icons/rules_icon.svg";
+
+  static const String imagesIconsShape = "assets/images/icons/shape.svg";
+
+  static const String imagesIconsUsageDataIcon =
+      "assets/images/icons/usage_data_icon.svg";
+
+  static const String imagesIdea = "assets/images/Idea.png";
+
+  static const String imagesInspection = "assets/images/Inspection.png";
+
+  static const String imagesMoneyBox = "assets/images/money_box.png";
+
   static const String imagesOnBoardingItem1 =
       "assets/images/on_boarding_item1.svg";
 
+  /// Assets for imagesOnBoardingItem2
+  /// assets/images/on_boarding_item2.svg
   static const String imagesOnBoardingItem2 =
       "assets/images/on_boarding_item2.svg";
 
@@ -13,21 +93,23 @@ class Assets {
 
   static const String imagesOnBoardingItem4 =
       "assets/images/on_boarding_item4.svg";
-  static const forgetPasswordAvatar =
-      'assets/images/forgot_password_re_hxwm 1.svg';
-  static const boxMoneyAvatar = 'assets/images/MoneyBox.png';
-  static const cigaretteAvatar = 'assets/images/cigarette.png';
-  static const goalAvatar = 'assets/images/Goal.png';
-  static const insepctionAvatar = 'assets/images/Inspection.png';
-  static const ideaAvatar = 'assets/images/Idea.png';
-  static const vedioTestimage = 'assets/images/vediotestimage.png';
-  static const articleTextImage = 'assets/images/articletestimage.png';
-  static const selfieAvatar = 'assets/images/selfieavatar.png';
-  static const warningAvatar = 'assets/images/warningAvatar.svg';
-  static const String imagesCigarette = "assets/images/cigarette.png";
-  static const String imagesError = "assets/images/error.png";
-  static const String imagesWallet = "assets/images/wallet.png";
+
   static const String imagesPencil = "assets/images/pencil.png";
+
   static const String imagesPlus = "assets/images/plus.png";
+
+  static const String imagesSelfieAvatar = "assets/images/selfie_avatar.png";
+
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  static const String imagesSplashLogo = "assets/images/splash_logo.png";
+
   static const String imagesTaskCompleted = "assets/images/task_completed.png";
+
+  static const String imagesVideoTestImage =
+      "assets/images/video_test_image.png";
+
+  static const String imagesWallet = "assets/images/wallet.png";
+
+  static const String imagesWarningAvatar = "assets/images/warningAvatar.svg";
 }
--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -1,77 +1,84 @@
 import 'package:flutter/material.dart';
 
 abstract class TextStyles {
-  static TextStyle bold24(context) {
+  static TextStyle bold24(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 24),
       fontWeight: FontWeight.w700,
     );
   }
 
-  static TextStyle bold16(context) {
+  static TextStyle bold20(BuildContext context) {
     return TextStyle(
-      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontSize: getResponsiveFontSize(context, fontSize: 20),
       fontWeight: FontWeight.w700,
     );
   }
 
-  static TextStyle bold18(context) {
+  static TextStyle bold18(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 18),
       fontWeight: FontWeight.w700,
     );
   }
 
-  static TextStyle bold12(context) {
+  static TextStyle bold16(BuildContext context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle bold12(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 12),
       fontWeight: FontWeight.w700,
     );
   }
 
-  static TextStyle medium28(context) {
+  static TextStyle medium28(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 28),
       fontWeight: FontWeight.w500,
     );
   }
 
-  static TextStyle medium24(context) {
+  static TextStyle medium24(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 24),
       fontWeight: FontWeight.w500,
     );
   }
 
-  static TextStyle medium20(context) {
+  static TextStyle medium20(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 20),
       fontWeight: FontWeight.w500,
     );
   }
 
-  static TextStyle medium18(context) {
+  static TextStyle medium18(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 18),
       fontWeight: FontWeight.w500,
     );
   }
 
-  static TextStyle medium16(context) {
+  static TextStyle medium16(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 16),
       fontWeight: FontWeight.w500,
     );
   }
 
-  static TextStyle medium14(context) {
+  static TextStyle medium14(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 14),
       fontWeight: FontWeight.w500,
     );
   }
 
-  static TextStyle medium12(context) {
+  static TextStyle medium12(BuildContext context) {
     return TextStyle(
       fontSize: getResponsiveFontSize(context, fontSize: 12),
       fontWeight: FontWeight.w500,
@@ -82,8 +89,10 @@ abstract class TextStyles {
 double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
   double scaleFactor = getScaleFactor(context);
   double responsiveFontSize = fontSize * scaleFactor;
+
   double lowerLimit = fontSize * 0.7;
   double upperLimit = fontSize * 1.2;
+
   return responsiveFontSize.clamp(lowerLimit, upperLimit);
 }
 
