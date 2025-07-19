4a9ed9ad305c1c2d5354425d49af9573f9700155
Ali kamar <aliekamar123@gmail.com>
2025-07-19 23:01:31 +0300

    add AchievedGoalsView and AchievedGoalsViewBody with item display for completed goals

---
+++ lib/features/savings_counter/presentation/views/widgets/achieved_goals_view_body.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nafas_app/core/utils/constant.dart';
+import 'package:nafas_app/features/savings_counter/presentation/views/widgets/achieved_goals_item.dart';
+
+import '../../manger/savings_counter_cubit/savings_counter_cubit.dart';
+
+class AchievedGoalsViewBody extends StatelessWidget {
+  const AchievedGoalsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kHorizontalPadding,
+      ),
+      child: ListView.builder(
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.only(
+            bottom: 12,
+          ),
+          child: AchievedGoalsItem(
+            currentGoalAmount:
+                context.read<SavingsCounterCubit>().state.completedGoals[index],
+          ),
+        ),
+        itemCount:
+            context.read<SavingsCounterCubit>().state.completedGoalsCount,
+      ),
+    );
+  }
+}
